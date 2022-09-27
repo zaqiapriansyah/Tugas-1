57ea06e5e867e489effbfecde90635cbf734b167
zaqiapriansyah <113027302+zaqiapriansyah@users.noreply.github.com>
2022-09-27 18:36:23 +0700

    Add files via upload

---
+++ luaslingkaran.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float luas, phi=3.14;
+  int r;
+  cout <<"Masukan Jari-Jari : ";
+  cin >> r;
+
+  luas=phi*r*r;
+
+  cout <<"Luas Lingkaran = "<<luas<<endl;
+}
---
+++ volumetabung.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float phi = 22/7;
+  int jari = 7;
+  int tinggi = 24;
+  cout << "Menghitung Volume Tabung" << endl;
+  cout << "Rumusnya phi x jari-jari x tinggi" << endl << endl;
+  cout << "Phi = " << phi << endl;
+  cout << "jari-jari = " << jari << endl;
+  cout << "tinggi = " << tinggi << endl;
+  
+  cout << "Hasilnhya = " << phi*jari-jari*tinggi << endl;
+
+  return 0;
+}
