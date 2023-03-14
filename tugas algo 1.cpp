41fb4034f95cb39ed19cc069d80fade06ba75a90
zaqiapriansyah <113027302+zaqiapriansyah@users.noreply.github.com>
2023-03-14 23:43:10 +0700

    Add files via upload

---
+++ tugas algo 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  // input data buku
+  int n;
+  cout << "Masukan jumlah buku: ";
+  cin >> n;
+  string buku[n];
+  for (int i = 0; i < n; i++){
+    cout << "Masukan judul buku ke-" << i+1 << ": ";
+    cin >> buku[i];
+  }
+
+  // Insertion Sort
+  for (int i = 1; i < n; i++){
+    string key = buku[i];
+    int j = i - 1;
+    while (j >= 0 && key < buku[j]) {
+      buku[j + 1] = buku[j];
+      j--;
+    }
+    buku[j + 1] = key;
+    }
+
+    // Output data buku yang sudah diurutkan
+    cout << "\nData buku yang sudah diurutkan:\n";
+    for (int i = 0; i < n; i++) {
+        cout << i+1 << ". " << buku[i] << endl;
+    }
+    return 0;
+  }
